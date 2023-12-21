e67b844ae6d037f36facbfd4b37658ebd8892bc9
PovoroznyukAlexandra <alexa.pvr2002@gmail.com>
2023-12-21 16:46:12 +0300

    GMRES_C++/GMRES MPI

--- GMRES_C++/GMRES MPI/GMRES_mpi.cpp
+++ GMRES_C++/GMRES MPI/GMRES_mpi.cpp
@@ -85,6 +85,46 @@ public:
 
         return result;
     }
+
+    std::vector<double> matvec_2(std::vector<double> &x_chunk, CBLAS_ORDER order = CblasRowMajor) {
+        std::vector<double> local_result(row);
+
+        int lda = (order == CblasRowMajor) ? col : row;
+        std::vector<int> x_sizes(size);
+        int tmp = x_chunk.size();
+        MPI_Allgather(&tmp, 1, MPI_INT, x_sizes.data(), 1, MPI_INT, MPI_COMM_WORLD);
+
+        auto matvec_chunk = x_chunk;
+
+        //cblas_dgemv(order, CblasNoTrans, row, col, 1.0, local_A.data(), lda, x.data(), 1, 1.0, local_result.data(), 1);
+
+        for (int shift = 1; shift < size; ++shift){
+            int send_proc_id = (rank + shift) % size, recv_proc_id = (rank - shift + size) % size;
+            std:: vector<double> next_chunk(x_sizes[recv_proc_id]);
+            MPI_Request send, recv;
+            MPI_Isend(x_chunk.data(), x_chunk.size(), MPI_DOUBLE, send_proc_id, shift, MPI_COMM_WORLD, &send);
+            MPI_Irecv(next_chunk.data(), next_chunk.size(), MPI_DOUBLE, recv_proc_id, shift, MPI_COMM_WORLD, &recv);
+            int block_start_col = (rank + shift - 1) % size;
+            std::vector<double> A_chunk(row * x_chunk.size());
+            for (int i = 0; i < row; ++i){
+                std::copy(local_A.begin() + i * col + block_start_col, local_A.begin() + i * col + block_start_col + x_chunk.size(), A_chunk.begin() + i * x_chunk.size());
+            }
+            cblas_dgemv(order, CblasNoTrans, row, x_chunk.size(), 1.0, A_chunk.data(), lda, matvec_chunk.data(), 1, 1.0, local_result.data(), 1);
+            //cblas_dgemv (order, CblasNoTrans, n, n, 1.0, &A_chunk(0, block_start_col * n), k, matvec_chunk.data(), 1, 1, y_chunk.data(), 1);
+            MPI_Status status;
+            MPI_Wait(&send, &status);
+            MPI_Wait(&recv, &status);
+            matvec_chunk = next_chunk;
+        }
+
+        //std::vector<double> result(matrix_rows);
+
+        //MPI_Gatherv(local_result.data(), row, MPI_DOUBLE, result.data(), counts_rcv.data(), shifts_rcv.data(),
+        //            MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        //MPI_Bcast(result.data(), result.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+        return local_result;
+    }
 };
 
 std::vector<double> Solve_Upper_Triangular(std::vector<double> &R, std::vector<double> &b, int n)
@@ -175,17 +215,18 @@ std::vector<double> GMRES(std::vector<double> &b, Matvec &matvec, double atol = 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    Matvec Q_(b.size(), 1, MPI_COMM_WORLD);
+    Matvec Q_(matvec.col, 1, MPI_COMM_WORLD);
 
-    auto norm_b = norm(b); // norm_b = norm(b)
+    double norm_b = squares(b);
+    MPI_Allreduce(MPI_IN_PLACE, &norm_b, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    norm_b = std::sqrt(norm_b); // norm_b = norm(b)
 
     std::vector<double> q(b.size());
 
     std::transform(b.begin(), b.end(), q.begin(), [norm_b](double value)
                    { return value / norm_b; }); // q = b / norm_b
-
-    MPI_Scatterv(q.data(), Q_.counts_snd.data(), Q_.shifts_snd.data(), MPI_DOUBLE, Q_.local_A.data(), q.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD); // разбили Q по процессам.
-
+    //MPI_Scatterv(q.data(), Q_.counts_snd.data(), Q_.shifts_snd.data(), MPI_DOUBLE, Q_.local_A.data(), q.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD); // разбили Q по процессам.
+    std::copy(q.begin(), q.end(), Q_.local_A.begin());
     std::vector<double> h((maxiter + 1) * maxiter, 0); // h = np.zeros((maxiter + 1, maxiter))
 
     std::vector<double> y;
@@ -198,20 +239,20 @@ std::vector<double> GMRES(std::vector<double> &b, Matvec &matvec, double atol = 
     for (n = 0; n < maxiter; ++n)
     {
         MPI_Bcast(&r, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-        MPI_Bcast(q.data(), q.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        //MPI_Bcast(q.data(), q.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-        auto v = matvec.matvec(q); // v = matvec(q)
+        auto v = matvec.matvec_2(q); // v = matvec(q)
 
-        std::vector<double> v_slice(v.begin() + Q_.shifts_snd[proc_id], v.begin() + Q_.shifts_snd[proc_id] + Q_.counts_snd[proc_id]);
+        //std::vector<double> v_slice(v.begin() + Q_.shifts_snd[proc_id], v.begin() + Q_.shifts_snd[proc_id] + Q_.counts_snd[proc_id]);
 
         for (int j = 0; j <= n; ++j)
         {
-            std::vector<double> tmp(v_slice.size(), 0);
+            std::vector<double> tmp(v.size(), 0);
             std::vector<double> Q_j(Q_.local_A.begin() + Q_.row * j, Q_.local_A.begin() + Q_.row * (j + 1));
 
             double local, global = 0;
 
-            local = cblas_ddot(Q_j.size(), Q_j.data(), 1, v_slice.data(), 1);
+            local = cblas_ddot(Q_j.size(), Q_j.data(), 1, v.data(), 1);
 
             MPI_Allreduce(&local, &global, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
             MPI_Bcast(&global, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -221,33 +262,32 @@ std::vector<double> GMRES(std::vector<double> &b, Matvec &matvec, double atol = 
             std::transform(Q_j.begin(), Q_j.end(), tmp.begin(), [&global](double value)
                            { return value * global; }); // h[j, n] * Q[:, j]
 
-            std::transform(v_slice.begin(), v_slice.end(), tmp.begin(), v_slice.begin(),
+            std::transform(v.begin(), v.end(), tmp.begin(), v.begin(),
                            [](double a, double b)
                            { return a - b; }); // v -= h[j, n] * Q[:, j]
 
-            MPI_Gatherv(v_slice.data(), v_slice.size(), MPI_DOUBLE, v.data(), Q_.counts_rcv.data(), Q_.shifts_rcv.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+//            MPI_Gatherv(v.data(), v.size(), MPI_DOUBLE, v.data(), Q_.counts_rcv.data(), Q_.shifts_rcv.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
             // MPI_Bcast(v.data(), v.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD)
         }
 
         // PARALLEL NORM OF v start
-        double h_n_1_n = 0;
-        double local = squares(v_slice);
-        MPI_Allreduce(&local, &h_n_1_n, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        double h_n_1_n = squares(v);
+        MPI_Allreduce(MPI_IN_PLACE, &h_n_1_n, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
         h_n_1_n = std::sqrt(h_n_1_n);
-        MPI_Bcast(&h_n_1_n, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        //  MPI_Bcast(&h_n_1_n, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         // PARALLEL NORM OF v end
 
         h[(n + 1) * maxiter + n] = h_n_1_n;
 
-        std::vector<double> q_slice(v_slice.size());
+        std::vector<double> q_slice(v.size());
 
-        std::transform(v_slice.begin(), v_slice.end(), q_slice.begin(), [&h_n_1_n](double value)
+        std::transform(v.begin(), v.end(), q_slice.begin(), [&h_n_1_n](double value)
                        { return value / h_n_1_n; });
 
         Q_.append(q_slice);
 
-        MPI_Gatherv(q_slice.data(), q_slice.size(), MPI_DOUBLE, q.data(), Q_.counts_rcv.data(), Q_.shifts_rcv.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+//        MPI_Gatherv(q_slice.data(), q_slice.size(), MPI_DOUBLE, q.data(), Q_.counts_rcv.data(), Q_.shifts_rcv.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
         if (proc_id == 0)
         {
@@ -327,34 +367,37 @@ int main(int argc, char **argv)
 
     int n = 20;
 
-    std::vector<double> b(n);
+    // std::vector<double> b(n);
+    int b_size = n / n_proc; // Число строк на каждом процессе
 
-    if (proc_id == 0)
+    if (proc_id == n_proc - 1)
     {
-        for (auto &x : b)
-        {
-            x = (-floatDist(gen));
-        }
+        b_size += n % n_proc;
     }
-    MPI_Bcast(b.data(), n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    std::vector<double> b(b_size);
+    for (auto &x : b)
+    {
+        x = (-floatDist(gen));
+    }
 
     Matvec matrix(n, n, MPI_COMM_WORLD);
 
     auto res = GMRES(b, matrix, t, 1e-6, n, proc_id, n_proc);
 
-    auto tmp = matrix.matvec(res);
+    auto tmp = matrix.matvec_2(res);
 
-    if (proc_id == 0)
-    {
-        std::transform(tmp.begin(), tmp.end(), b.begin(), tmp.begin(), [](double a, double b)
-                       { return a - b; });
-        std::cout << "||Ax - b|| = " << norm(tmp) << std::endl;
 
-        // for (auto x : res)
-        // {
-        //     std::cout << x << " ";
-        // }
-        // std::cout << std::endl;
+    std::transform(tmp.begin(), tmp.end(), b.begin(), tmp.begin(), [](double a, double b)
+                   { return a - b; });
+
+    double norm_res = squares(tmp);
+    MPI_Allreduce(MPI_IN_PLACE, &norm_res, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    norm_res = std::sqrt(norm_res);
+
+    if (proc_id == 0){
+        std::cout << "||Ax - b|| = " << norm_res << std::endl;
     }
 
     MPI_Finalize();
